728cce18ee1c59204e2aeb6b84f1b2894b5d56a4
kdoerges <kdoerges@cycronic.de>
2018-04-05 13:25:10 +0200

    initial check-in

---
+++ java/org/vebqa/vebtal/TestAdaptionPlugin.java
@@ -0,0 +1,15 @@
+package org.vebqa.vebtal;
+
+import javafx.scene.control.Tab;
+
+@SuppressWarnings("restriction")
+public interface TestAdaptionPlugin {
+
+	TestAdaptionType getType();
+	
+	String getName();
+	
+	Tab startup();
+	
+	boolean shutdown();
+}
---
+++ java/org/vebqa/vebtal/command/ICommand.java
@@ -0,0 +1,5 @@
+package org.vebqa.vebtal.command;
+
+public interface ICommand {
+
+}
